83236c040e652fab812bc6ce97cb3b32cc69042c
CleberSilva <clebercapricornio@gmail.com>
2024-02-15 18:18:45 -0300

    atualizei

---
+++ CSS/Javascript-013.js
@@ -0,0 +1,11 @@
+//criar uma matriz
+const cars = ["Saab", "Volvo", "BMW"];
+
+//Alterar um elemento:
+cars[0] = "Toyota";
+
+//Adicionar um elemento:
+cars.push('Audi');
+
+//Visualizar a matriz:
+document.getElementById("demo").innerHTML = cars;
