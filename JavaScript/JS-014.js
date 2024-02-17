1bed05bc4fa0fac3139109f787b1a296005f040b
CleberSilva <clebercapricornio@gmail.com>
2024-02-17 19:38:20 -0300

    atualizei

--- JavaScript/JS-014.js
+++ JavaScript/JS-014.js
@@ -32,3 +32,24 @@ let text1 = "A";
 let text2 = "B";
 let result = text1 < text2;
 document.getElementById("demo7").innerHTML = "A é menor do que B ? " + result;
+
+let text3 = "20";
+let text4 = "5";
+let result2 = text3 < text4;
+document.getElementById("demo8").innerHTML = "20 é menor do que 5 ? " + result2;
+
+let text5 = "Lancellotti";
+let text6 = "Escanor";
+let text7 = text5 + " " + text6;
+document.getElementById("demo9").innerHTML = text7;
+
+let text8 = "Que dia ";
+text8 += "tão bonito";
+document.getElementById("demo10").innerHTML = text8;
+
+let j = 5 + 5;
+let k = "5" + 5;
+let l = "Olá " + 5; 
+document.getElementById("demo11").innerHTML = j + "<br>" + k + "<br>" + l;
+
+// M N O P Q R S T U V W X Y Z
