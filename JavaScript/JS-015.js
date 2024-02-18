377da2eb8e6244251065dcfca7157ade840b95a3
CleberSilva <clebercapricornio@gmail.com>
2024-02-18 18:58:36 -0300

    atualizei

---
+++ JavaScript/JS-015.js
@@ -0,0 +1,23 @@
+let a = 100 + 50;
+document.getElementById("demo1").innerHTML = a;
+
+let b = 100;
+let c = 50;
+let d = b + c;
+document.getElementById("demo2").innerHTML = d;
+
+let e = 3;
+let f = (100+50) * e;
+document.getElementById("demo3").innerHTML = f;
+
+let g = 5;
+let h = 2;
+let i = g + h;
+document.getElementById("demo4").innerHTML = i;
+
+let j = 5;
+let k = 2;
+let l = j - k;
+document.getElementById("demo5").innerHTML = l;
+
+//A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
