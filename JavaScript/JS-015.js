a91b674a1de3484b432da21757fdb4d63b651de3
CleberSilva <clebercapricornio@gmail.com>
2024-02-19 18:21:32 -0300

    atualizei

--- JavaScript/JS-015.js
+++ JavaScript/JS-015.js
@@ -20,4 +20,42 @@ let k = 2;
 let l = j - k;
 document.getElementById("demo5").innerHTML = l;
 
+let m = 5;
+let n = 2;
+let o = m * n;
+document.getElementById("demo6").innerHTML = o;
+
+let p = 5;
+let q = 2;
+let r = p / q;
+document.getElementById("demo7").innerHTML = r;
+
+let s = 5;
+let t = 2;
+let u = s % t;
+document.getElementById("demo8").innerHTML = u;
+
+let v = 5;
+v++;
+let w = v;
+document.getElementById("demo9").innerHTML = w;
+
+let x = 5;
+x--;
+let y = x;
+document.getElementById("demo10").innerHTML = y;
+
+let z = 5;
+document.getElementById("demo11").innerHTML = z ** 2;
+
+let z2 = 5;
+document.getElementById("demo12").innerHTML = Math.pow(z2,2);
+
+document.getElementById("demo13").innerHTML = 100 + 50 * 3;
+
+document.getElementById("demo14").innerHTML = (100 + 50) * 3;
+
+document.getElementById("demo15").innerHTML = 100 + 50 - 3;
+
+document.getElementById("demo16").innerHTML = 100 / 50 * 3;
 //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
