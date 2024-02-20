ddd314906242c20bf23106425bd8d25a91e348e4
CleberSilva <clebercapricornio@gmail.com>
2024-02-20 17:28:08 -0300

    atualizei

--- JavaScript/JS-016.js
+++ JavaScript/JS-016.js
@@ -1,1 +1,72 @@
+let a = 10;
+document.getElementById("demo").innerHTML = "O valor de a é: " + a;
+
+let b = 50
+let c = 10 + b;
+document.getElementById("demo2").innerHTML = "O valor de c é: " + c;
+
+let d = 10;
+d += 5;
+document.getElementById("demo3").innerHTML = "O valor de d é: " + d;
+
+let text = "Olá";
+text += " Mundo";
+document.getElementById("demo4").innerHTML = text;
+
+let e = 10;
+e -= 5;
+document.getElementById("demo5").innerHTML = "O valor de e é: " + e;
+
+let f = 10;
+f *= 5;
+document.getElementById("demo6").innerHTML = "O valor de f: " + f;
+
+let g = 10;
+g **= 5;
+document.getElementById("demo7").innerHTML = "O valor de g é: " + g;
+
+let h = 10;
+h /= 5;
+document.getElementById("demo8").innerHTML = h;
+
+let i = 10;
+i %= 5;
+document.getElementById("demo9").innerHTML = "O valor de i é: " +i;
+
+let j = -100;
+j <<= 5;
+document.getElementById("demo10").innerHTML = "O valor de j é: " + j;
+
+let k = -100;
+k >>= 5;
+document.getElementById("demo11").innerHTML = "O valor de k é: " + k;
+
+let l = -100;
+l >>>= 5;
+document.getElementById("demo12").innerHTML = "O valor de l é: " + l;
+
+let m = 100;
+m &= 5;
+document.getElementById("demo13").innerHTML = "O valor de m é: " + m;
+
+let n = 100;
+n |= 5;
+document.getElementById("demo14").innerHTML = "O valor de n é: " + n;
+
+let o = 100;
+o ^= 5;
+document.getElementById("demo15").innerHTML = "O valor de o é: " + o;
+
+let p = 100;
+p &&= 5;
+document.getElementById("demo16").innerHTML = "O valoe de p é: " + p;
+
+let q = undefined;
+q ||= 5;
+document.getElementById("demo17").innerHTML = "O valor de q é: " + q;
+
+let r;
+document.getElementById("demo18").innerHTML = r ??= 5;
+
+
 //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
