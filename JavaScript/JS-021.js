8d646870dcd4579066cf27666c558a0312afc3e2
CleberSilva <clebercapricornio@gmail.com>
2024-03-01 19:41:16 -0300

    atualizei

--- JavaScript/JS-021.js
+++ JavaScript/JS-021.js
@@ -32,4 +32,46 @@ document.getElementById("demo7").innerHTML = text5;
 let text6 = "O carácter \\ é designado por barra invertida.";
 document.getElementById("demo8").innerHTML = text6;
 
+//Demo9
+document.getElementById('demo9').innerHTML = "Olá, Dolly!";
+
+//Demo10
+document.getElementById("demo10").innerHTML = "Olá," + " Dolly!";
+
+//Demo11
+let text7 = 
+`A rápida
+raposa castanha
+salta sobre
+o cão preguiçoso`;
+document.getElementById("demo11").innerHTML = text7;
+
+//Demo12
+// x é uma string
+let x = "João";
+
+// y é um objeto
+let y = new String("João");
+document.getElementById("demo12").innerHTML = typeof x + "<br>" + typeof y;
+
+//Demo13
+let a = "Jericó";              // a é uma string
+let b = new String("Jericó");  // b é um objeto
+document.getElementById("demo13").innerHTML = (a==b);
+
+//Demo14
+let c = "Jonas";  // c é uma string
+let d = new String("Jonas"); //d é um objeto
+document.getElementById("demo14").innerHTML = (c===d);
+
+//Demo15
+let e = new String("Junior");  // e é um objeto
+let f = new String("Junior"); // f é um objeto
+document.getElementById("demo15").innerHTML = (e==f);
+
+//Demo16
+let g = new String("Jonathan");  // g é um objeto
+let h = new String("Jonathan"); // h é um objeto
+document.getElementById("demo16").innerHTML = (g===h);
+
 //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
