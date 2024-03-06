90b4330958181a3dd614240589420ee6fb757f02
CleberSilva <clebercapricornio@gmail.com>
2024-03-06 15:05:39 -0300

    atualizei

--- JavaScript/JS-022.js
+++ JavaScript/JS-022.js
@@ -2,6 +2,22 @@
 let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 document.getElementById("demo").innerHTML = text.length;
 
+//DEMO-2
+var text2 = "OLÁ MUNDO";
+document.getElementById("demo2").innerHTML = text.charAt(0);
 
+//DEMO-3
+let text3 = "Olá mundo";
+document.getElementById("demo3").innerHTML = text3.charCodeAt(0)
+
+//DEMO-4
+const name = "W3Schools";
+let letter = name.at(2)
+document.getElementById("demo4").innerHTML = letter;
+
+//DEMO-5
+const name2 = "W3Schools";
+let letter2 = name[2];
+document.getElementById("demo5").innerHTML = letter;
 
 //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
