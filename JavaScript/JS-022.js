bd31d46bc947ecaa6f9786f6e1ce930ff2e1e9f0
CleberSilva <clebercapricornio@gmail.com>
2024-03-11 19:51:39 -0300

    terminei 11/03/2024

--- JavaScript/JS-022.js
+++ JavaScript/JS-022.js
@@ -27,6 +27,26 @@ document.getElementById("demo6").innerHTML = text[0];
 //DEMO-7
 let text5 = "Olá mundo! Demonstração 7°"
 text5[0] = "A";  //Não funciona
-document.getElementById("demo7").innerHTML = text5; 
+document.getElementById("demo7").innerHTML = text5;
+
+//DEMO-8
+let text6 = "Maçã, Banana, Kiwi";
+let part = text6.slice(6,12);
+document.getElementById("demo8").innerHTML = part;
+
+//DEMO-9
+let text7 = "Maçã, Banana, Kiwi";
+let part2 = text7.slice(6)
+document.getElementById("demo9").innerHTML = part2
+
+//DEMO-10
+let text8 = "Maçã, Banana, Kiwi";
+let part3 = text8.slice(-12);
+document.getElementById("demo10").innerHTML = part3;
+
+//DEMO-11
+let text9 = "Maçã, Banana, Kiwi";
+let part4 = text9.slice(-12,-6)
+document.getElementById("demo11").innerHTML = part4;
 
 //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
