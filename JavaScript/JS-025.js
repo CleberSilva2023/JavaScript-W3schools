d47b82955a90cce2b6c9d6672d835d09294edbe6
CleberSilva <clebercapricornio@gmail.com>
2024-05-01 18:17:58 -0300

    Terminei 01/05/2024

--- JavaScript/JS-025.js
+++ JavaScript/JS-025.js
@@ -3,6 +3,26 @@ let x = 3.14;
 let y = 3;
 document.getElementById("demo").innerHTML = x + "<br>" + y;
 
+//DEMO-02
+let a = 123e5;
+let b = 123e-5;
+document.getElementById("demo02").innerHTML = a + "<br>" + b;
 
+//DEMO-03
+let c = 999999999999999;
+let d = 9999999999999999;
+document.getElementById("demo03").innerHTML = c + "<br>" + d;
+
+//DEMO-04
+let e = 0.2 + 0.1;
+document.getElementById("demo04").innerHTML = "0.2 + 0.1 = " + e;
+
+//DEMO-05
+let f = 0.2 + 0.1;
+document.getElementById("demo05").innerHTML = "0.2 + 0.1 = " + f;
+
+//DEMO-06
+let g = (0.2*10 + 0.1*10) / 10;
+document.getElementById("demo06").innerHTML = "0.2 + 0.1 = " + g; //NÃO FUNCIONOU, ACHAR ERRO DEPOIS!!!
 
 //A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
